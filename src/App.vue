79469269b47ff0c53ecfd21207a847f16a9c7a79
enricoderasmo21 <derasmo.enrico@gmail.com>
2023-05-29 16:58:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppMain from './components/AppMain.vue'
+
+export default {
+  data() {
+    return{
+      
+    }
+  },
+
+  components: {
+  
+    AppMain,
+  
+  }
+}
+
+</script>
+
+<template>
+
+  <AppMain></AppMain>
+ 
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+  data() {
+    return{
+      
+    }
+  },
+
+  components: {
+  
+  }
+}
+
+</script>
+
+<template>
+ 
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
